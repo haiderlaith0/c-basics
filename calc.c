265d9bf071996285d21bb1add7086ffe24d64b54
HAISER02010 <medozhaidergaming@protonmail.com>
2025-10-14 21:30:53 +0300

    New C file: a new C file created for a challenge made by ChatGPT

---
+++ calc.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    float nums1;
+    float nums2;
+    float result;
+    char operator;
+
+    printf("Enter first number: ");
+    scanf("%f", &nums1);
+    printf("Enter second number: ");
+    scanf("%f", &nums2);
+    printf("Choose your operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    switch(operator) {
+        case '+':
+            result = nums1 + nums2;
+            printf("Result: %.2f\n", result);
+            break;
+        case '-':
+            result = nums1 - nums2;
+            printf("Result: %.2f\n", result);
+            break;
+        case '*':
+            result = nums1 * nums2;
+            printf("Result: %.2f\n", result);
+            break;
+        case '/':
+            if(nums2 != 0) {
+                result = nums1 / nums2;
+                printf("Result: %.2f\n", result);
+            } else {
+                printf("Error: Cannot divide by zero!\n");
+            }
+            break;
+        default:
+            printf("Error: Invalid operator!\n");
+    }
+
+    return 0;
+}

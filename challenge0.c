dd6fe7d79d1119c1e1f2cfa14f62e4de9d518216
HAISER02010 <medozhaidergaming@protonmail.com>
2025-10-15 15:08:31 +0300

    new C file: a file I made to go through a coding challenge Chat GPT gave to me

---
+++ challenge0.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+
+char grade;
+printf("What's your grade? ");
+scanf(" %c", &grade);
+
+
+switch(grade){
+case 'A':
+printf("Perfect score!");
+break;
+case 'B':
+printf("Your grade is %c", grade);
+break;
+case 'C':
+printf("Your grade is %c", grade);
+break;
+case 'D':
+printf("Your grade is %c", grade);
+break;
+case 'F':
+printf("Your grade is %c, Failed", grade);
+break;
+
+default: 
+printf("ERROR: Invalid grade!");
+}
+
+
+
+    return 0;
+}

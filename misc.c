a15f6a1638d8775b7356a068f37f6fb5d3711131
HAISER02010 <medozhaidergaming@protonmail.com>
2025-10-15 17:18:57 +0300

    new C file: added misc.c so I can experiment with it right after learning a new thing in C

---
+++ misc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    
+    double number = 64.0;
+    double result = pow(number, 64.0);
+    printf("The square root of %.2f is %.2f\n", number, result);
+
+
+    return 0;
+}

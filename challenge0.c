9a25ce4167ae3f2238566f3538a9d2c21abd389f
HAISER02010 <medozhaidergaming@protonmail.com>
2025-10-23 19:16:07 +0300

    fix: managed to fix this code in particular, not it works flawlessly

--- challenge0.c
+++ challenge0.c
@@ -2,41 +2,43 @@
 
 int main() {
 
-char grade;
+int grade;
 while (1){
-printf("To exit the program, please type 1.\n");
+printf("To exit the program, please type -1.\n");
 
-printf("What's your grade? ");
-scanf(" %c", &grade);
+printf("What's your grade? \n");
+scanf("%d", &grade);
 
-
-
-switch(grade){
-case 'A':
-printf("Perfect score!\n");
-break;
-case 'B':
-printf("Your grade is %c\n", grade);
-break;
-case 'C':
-printf("Your grade is %c\n", grade);
-break;
-case 'D':
-printf("Your grade is %c\n", grade);
-break;
-case 'F':
-printf("Your grade is %c, Failed\n", grade);
-break;
-
-default:
-printf("ERROR: Invalid grade!\n");
-}
-
-
-if (grade == '1'){
+if (grade == -1){
     printf("Exitting program...\n");
     break;
 }
+
+
+
+if(grade == 100){
+    printf("Perfect score! A+\n");
+}
+
+else if(grade >= 90){
+printf("Excellent! A\n");
+}
+else if(grade >= 80){
+printf("Good! B\n");
+}
+else if(grade >= 70){
+printf("Average. C\n");
+}
+else if(grade >= 60){
+printf("Below Average. D\n");
+}
+else if(grade >= 0){
+
+    printf("Failed! F\n");
+}else{
+    printf("Invalid grade!\n");
+}
+
 
 }
 

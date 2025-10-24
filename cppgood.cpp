48a26485640e4f1c47d5faa060196235e4945bbc
HAISER02010 <medozhaidergaming@protonmail.com>
2025-10-24 15:53:39 +0300

    addition and update to my progress: I began to code in C++ despite not watching any tutorials about it, just because I know how to code in C... Now I know why people love C++, I'll continue learning C anyways

---
+++ cppgood.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+double converterBody(double temp, char conversionMethod){
+
+switch(conversionMethod){
+    case 'A':
+    return (temp * 9.0 / 5.0) + 32;
+    break;
+    case 'B':
+    return (temp - 32) * 5.0 / 9.0;
+    break;
+
+    default:
+    std::cout << "ERROR: Invalid conversion method!" << std::endl;
+    return temp;
+}
+
+return 0;
+}
+
+int main(){
+
+ int exitVariable; double temp = 0; double convertedTemp; char conversionMethod;
+
+ while(1){
+
+    std::cout << "To exit, please type 0. If not, just enter 1." << std::endl;
+    std::cin >> exitVariable;
+
+    if(exitVariable == 0){
+        std::cout << "Got it. Quitting..." << std::endl;
+        break;
+    }
+
+ std::cout << "To convert from Celsius to Fahrenheit, enter A. To convert from Fahrenheit to Celsius, enter B" << std::endl;
+ std::cin >> conversionMethod;
+
+ std::cout << "How much is the temperature? " << std::endl;
+ std::cin >> temp;
+
+    convertedTemp = converterBody(temp, conversionMethod);
+    std::cout << "The converted temperature is: " << convertedTemp << std::endl;
+
+ }
+    return 0;
+}

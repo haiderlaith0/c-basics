9728425007415f2d4679f8384c36af90e6a13cde
unknown <medozhaidergaming@protonmail.com>
2025-11-09 00:34:49 +0300

    progression: made a number guessing game, it is difficult but i am starting to get it

--- learningrandoms.c
+++ learningrandoms.c
@@ -6,9 +6,30 @@ int main(){
 
     //! printf("%d", rand());
 
+
     srand(time(NULL));
 
-    printf("The random number is: %d", rand());
+    int min = 1;
+    int max = 100;
+    int userGuess;
+
+    int randNums = (rand() % (max - min + 1)) + min;
+
+    printf("*** WELCOME TO THE NUMBER GUESSING GAME!\n");
+
+    do{printf("Enter a random number: \n");
+    scanf("%d", &userGuess);
+
+    if(userGuess < randNums){
+        printf("Too low!\n");
+    }else if(userGuess > randNums){
+        printf("Too high!\n");
+    }else{
+        printf("Correct!\n");
+    }
+    }while(userGuess != randNums);
+
+    printf("The correct answer is: %d", randNums);
 
     return 0;
 

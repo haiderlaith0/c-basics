9e29fdfefde2c32e60b322e625b57258446ad7ac
Mero Gaming <medozhaidergaming@protonmail.com>
2025-11-11 20:12:25 +0300

    update: now we have two arrays! one stores numbers from 0 to 9, the other is randomly filled with digits from the first array (which is named mainnums)

--- myideaofrandomness.c
+++ myideaofrandomness.c
@@ -1,23 +1,34 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdio.h>   // For input/output functions like printf
+#include <unistd.h>  // For getpid() to get the process ID
+#include <stdlib.h>  // For srand(), rand(), and other utility functions
+#include <time.h>    // For time() to seed the random number generator
 
+int main() {
+    // Array containing the digits 0-9
+    int mainnums[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
 
-int main(){
-    int nums[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-    srand(time(NULL) ^ getpid() ^ (uintptr_t)&nums);
-    
-    int minimum = 0;
-    int maximum = sizeof(nums) / sizeof(nums[0]);
-    int randomchoice;
+    // Seed the random number generator using a combination of the current time,
+    // process ID, and the address of mainnums to ensure randomness
+    srand(time(NULL) ^ getpid() ^ (uintptr_t)&mainnums);
 
-    
+    // Array to store 16 randomly picked digits from mainnums
+    int randomnums[32];
 
-    for(int i = 0; i < maximum; i++){
-        randomchoice = (rand() % maximum) + minimum;
-        printf("%d", nums[randomchoice]);
+    // Define the range for random number generation
+    int sizeofarray = sizeof(randomnums) / 4; // The size of the array named randomnums
+    int randomchoice; // Variable to store the randomly chosen index
+
+    // Loop to fill randomnums with random digits from mainnums
+    for (int i = 0; i < sizeofarray; i++) {
+        // Generate a random index between 0 and 9
+        randomchoice = rand() % 10;
+
+        // Assign the randomly chosen digit from mainnums to randomnums
+        randomnums[i] = mainnums[randomchoice];
+
+        // Print the randomly chosen digit
+        printf("%d", randomnums[i]);
     }
 
-    return 0;
+    return 0; // Exit the program
 }

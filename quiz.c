73e47621f89ba66e7dca8e731ba71a046c921444
Mero Gaming <medozhaidergaming@protonmail.com>
2025-11-14 04:22:59 +0300

    MAJOR PROGRESSION: I finally managed to reach a state of progression where I could code this quiz game in C. This is by far one of the projects I'm most proud of...

---
+++ quiz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+
+    char questions[3][200] = {"What is the largest planet in the solar system?\n",
+                             "What is the hottest planet in the solar system?\n",
+                             "What's the closest planet to the sun?\n"};
+    char options[][200] = {"A. Mercury\n B. Venus\n C. Earth\n D. Mars\n E. Jupiter\n F. Saturn\n G. Uranus\n H. Neptune\n"};
+    char answers[] = {'E', 'B', 'A'};
+    int score = 0;
+    char guess = '\0';
+
+    int generalPurposeSize = sizeof(questions) / sizeof(questions[0]);
+
+    for(int i = 0; i < generalPurposeSize; i++){
+        printf("\n%s\n", questions[i]);
+        printf("\n%s\n", options[0]);
+        puts("Enter your choice: ");
+        scanf(" %c", &guess);
+
+        if(guess == answers[i]){
+            score+=1;
+            puts("\nCorrect answer!\n");
+        }else{
+            printf("WRONG! The answer is %c\n", answers[i]);
+        }
+    }
+    printf("Your score is: %d", score);
+
+    return 0;
+}
